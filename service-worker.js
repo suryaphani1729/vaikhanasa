2f9715611857a82dd2b6cf9f0b18abe16bb8cf9c
surya <surya.phani1729@gmail.com>
2019-06-13 14:53:09 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,49 @@
-// Flag for enabling cache in production
-var doCache = false;
+var cacheName = 'pwa-app-cache';
 
-var CACHE_NAME = 'pwa-app-cache';
 
-// Delete old caches
-self.addEventListener('activate', event => {
-  const currentCachelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys()
-      .then(keyList =>
-        Promise.all(keyList.map(key => {    
-          if (!currentCachelist.includes(key)) {
-            return caches.delete(key);
-          }
-        }))
-      )
+var filesToCache = [
+   '/vaikhanasa/',
+  '/vaikhanasa/index.html'
+];
+
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
   );
 });
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  console.log('requestt',e.request);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+
 
 // This triggers when user starts the app
 self.addEventListener('install', function(event) {
